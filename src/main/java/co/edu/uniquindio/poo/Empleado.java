71f032f0503691a0141b41ca07382c95daa90b39
Carlos Ruiz <carlosa.ruiza@uqvirtual.edu.co>
2024-10-07 22:42:49 -0500

    Creacion del proyecto

---
+++ src/main/java/co/edu/uniquindio/poo/Empleado.java
@@ -0,0 +1,33 @@
+package co.edu.uniquindio.poo;
+
+public abstract class Empleado {
+    private String nombre, identificacionñ;
+
+    public abstract void calcularSalario();
+
+    public Empleado(String nombre, String identificacionñ) {
+        this.nombre = nombre;
+        this.identificacionñ = identificacionñ;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacionñ() {
+        return identificacionñ;
+    }
+
+    public void setIdentificacionñ(String identificacionñ) {
+        this.identificacionñ = identificacionñ;
+    }
+
+    @Override
+    public String toString() {
+        return "Empleado [nombre=" + nombre + ", identificacionñ=" + identificacionñ + "]";
+    }
+}
