dc4b03abfd1641289761637aa1437506938cefde
Carlos Ruiz <carlosa.ruiza@uqvirtual.edu.co>
2024-10-09 23:15:58 -0500

    Creacion de los metodos de la clase y los test

--- src/main/java/co/edu/uniquindio/poo/EmpleadoTiempoCompleto.java
+++ src/main/java/co/edu/uniquindio/poo/EmpleadoTiempoCompleto.java
@@ -1,5 +1,34 @@
 package co.edu.uniquindio.poo;
+import java.util.Scanner;
 
-public class EmpleadoTiempoCompleto {
-    
+public class EmpleadoTiempoCompleto extends Empleado{
+
+    private double salarioMensual;
+        
+    public EmpleadoTiempoCompleto(String nombre, String identificacionñ, double salarioMensual) {
+        super(nombre, identificacionñ);
+        this.salarioMensual = salarioMensual;
+    }
+
+    @Override
+    public double calcularSalario() {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese el salario del empleado de tiempo completo: ");
+        double salarioMensual = teclado.nextDouble();
+        System.out.println("El salario del empleado de tiempo completo es: "+ salarioMensual);
+        return salarioMensual;
+    }
+
+    public double getSalarioMensual() {
+        return salarioMensual;
+    }
+
+    public void setSalarioMensual(double salarioMensual) {
+        this.salarioMensual = salarioMensual;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpleadoTiempoCompleto ["+ super.toString() +"]";
+    }
 }
---
+++ src/test/java/co/edu/uniquindio/poo/TestEmpleadoTiempoCompleto.java
@@ -0,0 +1,22 @@
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+public class TestEmpleadoTiempoCompleto {
+    private static final Logger LOG = Logger.getLogger(AppTest.class.getName());
+    /**
+     * Test AssertEquals
+     */
+    @Test
+    public void testAssertEquals() {
+        LOG.info("Inicio test Salario del empleado de tiempo completo");
+
+        EmpleadoTiempoCompleto empleado = new EmpleadoTiempoCompleto("Carlos", "123456", 2000000);
+        double salario = empleado.calcularSalario();
+        System.out.println("Prueba"+salario);
+        assertEquals(2000000, salario);
+        LOG.info("Fin test assertEquals");
+    }
+}
